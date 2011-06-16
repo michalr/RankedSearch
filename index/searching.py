b28096d73c907a3839b86d55d6b88e4c76257096
Michał Rychlik <michal.rychlik21@op.pl>
2011-06-16 17:41:01 +0200

    Searching fixed.

--- index/searching.py
+++ index/searching.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-from math import sqrt
+from math import sqrt, log10
 import re
 
 def vectors_sim(v1, v2):
@@ -32,9 +32,9 @@ def parse_phrase(raw_phrase, morfologik_object):
         bases.append(base_forms)
     return bases
 
-def search_phrase(phrase, index, morfologik_object):
+def search_phrase(phrase, index, morfologik_object, df, indexed_articles):
     bases = parse_phrase(phrase, morfologik_object)
-    res = bases[0]
+    res = [bases[0]]
     for base_forms in bases[1:]:
         next = []
         for elem in res:
@@ -43,15 +43,25 @@ def search_phrase(phrase, index, morfologik_object):
         res = next
     vectors_to_rank = []
     for elem in res:
-        vectors_set = set(index[elem[0]])
+        vectors_set = index[elem[0]]
         for base in elem[1:]:
-            vectors_set &= index[base]
-        vectors_to_rank += [(elem, vectors_set)]
-    ranking = []
+            keys = [article[0] for article in vectors_set]
+            new_vectors_set = []
+            for article in index[base]:
+                if article[0] in keys:
+                    new_vectors_set += [article]
+            vectors_set = new_vectors_set
+            # create tf_idf vector for query
+            elem_vector = {}
+            for word in elem:
+                elem_vector[word] = elem_vector.setdefault(word, 0) + 1
+            for word in elem_vector.keys():
+                elem_vector[word] *= log10((indexed_articles + 1) / (df[word] + 1))
+        vectors_to_rank += [(elem_vector, vectors_set)]
+    ranking = {}
     for vector_set in vectors_to_rank:
-        local_results = []
         for vec in vector_set[1]:
-            local_results.append((vec[0], vectors_sim(vector_set[0], vec[1])))
-        ranking.extend(local_results)
-    return [article[0] for article in sorted(ranking, key = lambda res: res[1], reverse = True)]
+            vector_set[0]
+            ranking[vec[0]] = max(ranking.setdefault(vec[0], -1), vectors_sim(vector_set[0], vec[1]))
+    return [article[0] for article in sorted(ranking.iteritems(), key=lambda (k,v): (v,k))]
         
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 from tools.file_parsing import parse_articles, read_morfologik
 from index.creation import create_index
+from index.searching import search_phrase
 
 if __name__ == '__main__':
     input = open("wikipedia_short.txt", "rt")
@@ -8,5 +9,8 @@ if __name__ == '__main__':
     morfologik_object = read_morfologik(morfologik_file)
     morfologik_file.close()
     art_dict, df = parse_articles(input, morfologik_object)
+    indexed_articles = len(art_dict.keys())
     input.close()
     index = create_index(art_dict, df)
+    result = search_phrase("mistrz klawiatury", index, morfologik_object, df, indexed_articles)
+    print result
